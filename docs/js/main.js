ecb44525f2350fc2a67d02235b1ef38239f575b7
Юрий Ронин <47382042+yurkaronin@users.noreply.github.com>
2022-01-26 16:23:56 +0300

    Добавил плавный скролл страниц Вынес подключение скриптов в отдельный шаблон

--- docs/js/main.js
+++ docs/js/main.js
@@ -35,13 +35,40 @@ if (menuButton) {
 menuButton.addEventListener('click', function (е) {
   if (menuButton.classList.contains('active')) {
     headerLogo.src = 'img/logo-rus.svg';
-    console.log("Меняем на чёрный!");
   } else {
     if (document.location.pathname === '/home.html') {
       headerLogo.src = 'img/logo-rus--wh.svg';
     }
 
   }
+});
+
+SmoothScroll({
+  // Время скролла 400 = 0.4 секунды
+  animationTime: 800,
+  // Размер шага в пикселях
+  stepSize: 75,
+
+  // Дополнительные настройки:
+
+  // Ускорение
+  accelerationDelta: 30,
+  // Максимальное ускорение
+  accelerationMax: 2,
+
+  // Поддержка клавиатуры
+  keyboardSupport: true,
+  // Шаг скролла стрелками на клавиатуре в пикселях
+  arrowScroll: 50,
+
+  // Pulse (less tweakable)
+  // ratio of "tail" to "acceleration"
+  pulseAlgorithm: true,
+  pulseScale: 4,
+  pulseNormalize: 1,
+
+  // Поддержка тачпада
+  touchpadSupport: true,
 });
 
 
