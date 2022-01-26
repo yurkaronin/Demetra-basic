3e6f89465ebf544f9a08e2acefaa78f9128676af
Юрий Ронин <47382042+yurkaronin@users.noreply.github.com>
2022-01-26 15:16:29 +0300

    Правки в контейнерах Оживление меню на JS Правки в активных классах у элементов DOM Оживление замены логотипа в шапке при активном и пассивном меню Рефакторинг стилей для мобильного меню

--- docs/js/main.js
+++ docs/js/main.js
@@ -12,3 +12,37 @@ window.onscroll = function showHeader() {
     headerLogo.src = 'img/logo-rus--wh.svg';
   }
 }
+
+// кнопка меню
+var menuButton = document.querySelector('.button-menu');
+var headerLogo = document.querySelector('.logo__image');
+
+if (menuButton) {
+  menuButton.addEventListener('click', function (r) {
+    document.body.classList.toggle('custom-lock');
+    document.body.classList.toggle('active');
+    menuButton.classList.toggle('active');
+  });
+
+}
+
+// Подмена лого в шапке, при открытом мобильном меню
+menuButton.addEventListener('click', function (е) {
+  if (menuButton.classList.contains('active')) {
+    headerLogo.src = 'img/logo-rus.svg';
+    console.log("Меняем на чёрный!");
+  } else {
+    headerLogo.src = 'img/logo-rus--wh.svg';
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
