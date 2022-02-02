1b68c6fe0ce2971cba768eca68def1b66ce98bad
Юрий Ронин <47382042+yurkaronin@users.noreply.github.com>
2022-02-03 02:06:27 +0300

    Рефакторинг последовательности скриптов, последовательности загрузки шрифтов, переподключил шрифты на страницах

--- docs/js/main.js
+++ docs/js/main.js
@@ -87,35 +87,6 @@ logotypeLink.addEventListener('mouseout', function (e) {
   footerLogo.src = 'img/logo-rus--wh.svg';
 });
 
-// Кастомная валидация форм
-(function () {
-  window.addEventListener("click", function (event) {
-
-    if (event.target.classList.contains("js-validate")) {
-
-      const formParent = event.target.closest("form");
-
-      formParent.querySelectorAll(".custom-form__item-wrapper").forEach(function (item) {
-        console.log(item)
-        if (item.querySelector("[data-required]")) {
-          if (item.querySelector("[data-required]").value === '') {
-            console.log("не заполнен")
-            item.classList.add("js-field-error");
-          } else {
-            console.log("заполнен")
-            item.classList.remove("js-field-error");
-          }
-        }
-      });
-    }
-  })
-})();
-
-// маска для поля с телефоном
-Inputmask("+7 (999) 999-99-99").mask('[type="tel"]');
-
-
-
 
 
 
