31cbf761cde2227bc847e1ccff40312909bbb404
Юрий Ронин <47382042+yurkaronin@users.noreply.github.com>
2022-02-05 15:56:21 +0300

    Адаптив личного кабинета + JS для табов

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,48 +1,55 @@
-// const container = document.querySelector(".custom-price");
-// const btns2 = document.querySelectorAll(".tabs__buttons");
-// const content = document.querySelectorAll(".custom-price__tabs-item");
-//
-// container.addEventListener("click", function (e) {
-//   const id = e.target.dataset.id;
-//   if (id) {
-//     btns2.forEach(function (btn) {
-//       btn.classList.remove("active");
-//       e.target.classList.add("active");
-//     });
-//     content.forEach(function (tabs) {
-//       tabs.classList.remove("active");
-//     });
-//     const element = document.getElementById(id);
-//     element.classList.add("active");
-//   }
-// });
+if (document.querySelectorAll("#js-price-section")) {
+  const tabsBtn = document.querySelectorAll(".tabs__buttons");
+  const tabsItems = document.querySelectorAll(".custom-price__tabs-item");
 
-const tabsBtn   = document.querySelectorAll(".tabs__buttons");
-const tabsItems = document.querySelectorAll(".custom-price__tabs-item");
+  tabsBtn.forEach(onTabClick);
 
-tabsBtn.forEach(onTabClick);
+  function onTabClick(item) {
+    item.addEventListener("click", function () {
+      let currentBtn = item;
+      let tabId = currentBtn.getAttribute("data-tab");
+      let currentTab = document.querySelector(tabId);
 
-function onTabClick(item) {
-  item.addEventListener("click", function() {
-    let currentBtn = item;
-    let tabId = currentBtn.getAttribute("data-tab");
-    let currentTab = document.querySelector(tabId);
+      if (!currentBtn.classList.contains('active')) {
+        tabsBtn.forEach(function (item) {
+          item.classList.remove('active');
+        });
 
-    if( ! currentBtn.classList.contains('active') ) {
-      tabsBtn.forEach(function(item) {
-        item.classList.remove('active');
-      });
+        tabsItems.forEach(function (item) {
+          item.classList.remove('active');
+        });
 
-      tabsItems.forEach(function(item) {
-        item.classList.remove('active');
-      });
-
-      currentBtn.classList.add('active');
-      currentTab.classList.add('active');
-    }
-  });
+        currentBtn.classList.add('active');
+        currentTab.classList.add('active');
+      }
+    });
+  }
 }
 
-// document.querySelector('.tabs__nav-btn').click();
+if (document.querySelectorAll("#js-cabinet-tabs")) {
+  const tabsBtn = document.querySelectorAll(".cabinet-tabs__navigation-link");
+  const tabsItems = document.querySelectorAll(".cabinet-tabs__tab-details");
 
+  tabsBtn.forEach(onTabClick);
 
+  function onTabClick(item) {
+    item.addEventListener("click", function () {
+      let currentBtn = item;
+      let tabId = currentBtn.getAttribute("data-tab");
+      let currentTab = document.querySelector(tabId);
+
+      if (!currentBtn.classList.contains('active')) {
+        tabsBtn.forEach(function (item) {
+          item.classList.remove('active');
+        });
+
+        tabsItems.forEach(function (item) {
+          item.classList.remove('active');
+        });
+
+        currentBtn.classList.add('active');
+        currentTab.classList.add('active');
+      }
+    });
+  }
+}
